69c33a4f2308582eec55e77ed467d2efb56e9dc4
Yunting <tsnoy23694549@gmail.com>
2018-10-12 17:56:28 +0800

    降低內存

--- app/src/main/java/com/example/user/timekeeper_testtest/BootService.java
+++ app/src/main/java/com/example/user/timekeeper_testtest/BootService.java
@@ -6,12 +6,15 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
 
 import java.util.Calendar;
 
 public class BootService extends Service {
+    Handler handler = new Handler();
+
     public BootService() {
     }
 
@@ -30,39 +33,48 @@ public class BootService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("service","!!!!");
-        DB_normal_alarm db = new DB_normal_alarm(this);
-        //cursor
-        if (db != null) {
-            Cursor cursor = db.select();
-            if (cursor.getCount() > 0) {
-                int i = 0;
-                for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
-                    Calendar cal = Calendar.getInstance();
-                    Long t = Long.parseLong(cursor.getString(6));
-                    cal.setTimeInMillis(t);
-                    Boolean ifrepeat = Boolean.parseBoolean(cursor.getString(4));
-                    AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                    Intent intent1 = new Intent(this, normal_alarmalert.class);
-                    intent1.putExtra("requestcode", cursor.getInt(3));
-                    PendingIntent pi1 = PendingIntent.getActivity(this, cursor.getInt(3), intent1, PendingIntent.FLAG_UPDATE_CURRENT);
-                    if (ifrepeat) {
-                        Log.d("case", ":repear");
-                        alarm.setRepeating(AlarmManager.RTC_WAKEUP, Long.parseLong(cursor.getString(6)), 24 * 60 * 60 * 1000, pi1);
-                    } else {
-                        if (System.currentTimeMillis() > Long.parseLong(cursor.getString(6))) {
-                            Log.d("case", ":settmr");
-                            alarm.setExact(AlarmManager.RTC_WAKEUP, Long.parseLong(cursor.getString(6)) + 24 * 60 * 60 * 1000, pi1);
-                        } else {
-                            Log.d("case", ":settoday");
-                            alarm.setExact(AlarmManager.RTC_WAKEUP, Long.parseLong(cursor.getString(6)), pi1);
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.d("service","!!!!");
+                DB_normal_alarm db = new DB_normal_alarm(BootService.this);
+                //cursor
+                if (db != null) {
+                    Cursor cursor = db.select();
+                    if (cursor.getCount() > 0) {
+                        int i = 0;
+                        for (cursor.moveToFirst(); !cursor.isAfterLast(); cursor.moveToNext()) {
+                            Calendar cal = Calendar.getInstance();
+                            Long t = Long.parseLong(cursor.getString(6));
+                            cal.setTimeInMillis(t);
+                            Boolean ifrepeat = Boolean.parseBoolean(cursor.getString(4));
+                            AlarmManager alarm = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                            Intent intent1 = new Intent(BootService.this, normal_alarmalert.class);
+                            intent1.putExtra("requestcode", cursor.getInt(3));
+                            PendingIntent pi1 = PendingIntent.getActivity(BootService.this, cursor.getInt(3), intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+                            if (ifrepeat) {
+                                Log.d("case", ":repear");
+                                alarm.setRepeating(AlarmManager.RTC_WAKEUP, Long.parseLong(cursor.getString(6)), 24 * 60 * 60 * 1000, pi1);
+                            } else {
+                                if (System.currentTimeMillis() > Long.parseLong(cursor.getString(6))) {
+                                    Log.d("case", ":settmr");
+                                    alarm.setExact(AlarmManager.RTC_WAKEUP, Long.parseLong(cursor.getString(6)) + 24 * 60 * 60 * 1000, pi1);
+                                } else {
+                                    Log.d("case", ":settoday");
+                                    alarm.setExact(AlarmManager.RTC_WAKEUP, Long.parseLong(cursor.getString(6)), pi1);
+                                }
+                            }
+                            i++;
                         }
                     }
-                    i++;
                 }
+                db.close();
+                handler.postDelayed(this, 300000);
             }
-        }
-        db.close();
+        };
+
+        runnable.run();
+
         return super.onStartCommand(intent, flags, startId);
     }
 
